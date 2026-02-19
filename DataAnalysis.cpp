8d0ab4705274442b0d73690a1bb2f4c32aceae63
JakeRees <jakerees04@gmail.com>
2026-02-19 15:05:00 +0000

    Can also sort by mark, added ReadMe and Title

--- DataAnalysis.cpp
+++ DataAnalysis.cpp
@@ -1,3 +1,11 @@
+/*
+                              Jake Rees, 11307374
+                           University of Manchester
+
+This program reads, analyses and outputs a formatted statistical analysis on
+student mark data from a given .dat file.
+*/
+
 #include<iostream>
 #include<iomanip>
 #include<cmath>
@@ -13,34 +21,27 @@ using std::vector;
 
 struct Course 
 {
-    string name;
-    double mark;
-    int id;
+  string name;
+  double mark;
+  int id;
 
-    Course(string name, double mark, int id)
-    {
-        this->name = name;
-        this->mark = mark;
-        this->id = id;
-    }
+  Course(string name, double mark, int id)
+  {
+    this->name = name;
+    this->mark = mark;
+    this->id = id;
+  }
 };
 
 double get_standard_deviation(vector<Course>& data, double mean, int size)
 {
-    // Takes a vector and returns the standard deviation of the elements
-    double standard_deviation = 0.0;
+  // Takes a vector and returns the standard deviation of the elements
+  double standard_deviation = 0.0;
 
-    for (int i = 0; i < size; ++i) {
-        standard_deviation += pow(data[i].mark - mean, 2);
-    }
+  for (int i = 0; i < size; ++i)
+    standard_deviation += pow(data[i].mark - mean, 2);
 
-    return sqrt(standard_deviation / size);
-}
-
-bool compare_courses(Course a, Course b) 
-{
-    // Compares course names
-    return a.name.compare(b.name) < 0;
+  return sqrt(standard_deviation / size);
 }
 
 int get_integer_input(string& message) 
@@ -62,7 +63,7 @@ int get_integer_input(string& message)
   }
 }
 
-char get_char_input(string message, char &extra)
+char get_char_input(string message, char& extra)
 {
   /* Retrieves a character input and converts to lowercase, 
   also retrieves second character of input for validation purposes */
@@ -76,151 +77,155 @@ char get_char_input(string message, char &extra)
   return input;
 }
 
-vector<Course> read_file(string file_name)
+vector<Course> read_file(string file_name, vector<int>& bad_lines)
 {
-    /* Open a file and read the coloumns into three seperate vectors, 
-    which are then stored using a struct */
-    vector<double> mark_data;
-    vector<int> course_id;
-    vector<string> course_name;
+  /* Open a file and read the coloumns into three seperate vectors, 
+  which are then stored using a struct */
+  std::ifstream file(file_name);
+  if (!file) 
+  { 
+    std::cerr << "\033[1;31mFailed to open file\033[0m\n";
+    abort();
+  }
 
-    std::ifstream file(file_name);
-    if (!file) 
-    { 
-        std::cerr << "\033[1;31mFailed to open file\033[0m\n";
-    }
+  vector<Course> courses;
+  int line_number = 0;
+  string line;
 
-    vector<Course> courses;
-    vector<int> bad_lines;
-    int line_number = 0;
+  while (std::getline(file, line))
+  {
+    line_number += 1;
+    std::istringstream converter(line);
 
-    string line;
+    double mark;
+    int id;
+    string name;
 
-    while (std::getline(file, line))
+    // Ensures data is of expected type (double, int, string)
+
+    if (converter >> mark >> id)
     {
-        line_number += 1;
-        std::istringstream converter(line);
+      std::getline(converter, name);
 
-        double mark;
-        int id;
-        string name;
+      // Remove leading whitespace
+      name.erase(0, name.find_first_not_of(" "));
 
-        // Ensures data is of expected type (double, int, string)
-
-        if (converter >> mark >> id)
-        {
-            std::getline(converter, name);
-
-            // Remove leading whitespace
-            name.erase(0, name.find_first_not_of(" "));
-
-            courses.push_back(Course(name, mark, id));
-        }
-        else
-        {
-            bad_lines.push_back(line_number);
-        }
+      courses.push_back(Course(name, mark, id));
     }
-    
-    if (bad_lines.size() > 0)
+    else
     {
-        cout << "\033[1;33mWarning: Couldn't read data from following lines: ";
-        for (int i = 0; i < bad_lines.size(); i++)
-        {
-            cout << bad_lines[i] << ", ";
-        }
-
-        cout << "\033[0m\n";
+        bad_lines.push_back(line_number);
     }
+  }
 
-
-    return courses;
+  return courses;
 }
 
 void analyse_data(vector<Course>& data)
 {
-    /* Takes a vector of course data and performs a 
-    generic statistical analysis on marks of said data */
-    double sum = 0.0;
-    int size = data.size();
+  /* Takes a vector of course data and performs a 
+  basic statistical analysis on marks of said courses */
+  double sum = 0.0;
+  int size = data.size();
 
-    for (int i = 0; i < size; ++i) {
-        sum += data[i].mark;
-    }
+  for (int i = 0; i < size; ++i)
+    sum += data[i].mark;
 
-    double mean = sum / size;
-    
-    double standard_deviation = get_standard_deviation(data, mean, size);
-    double standard_error = standard_deviation/sqrt(size);
+  double mean = sum / size;
+  
+  double standard_deviation = get_standard_deviation(data, mean, size);
+  double standard_error = standard_deviation/sqrt(size);
 
-    cout  << "\nMean average of marks: " << mean;
-    cout << "\nStandard deviation of marks: " << standard_deviation;
-    cout << "\nStandard error of marks: " << standard_error;
+  cout  << "\nMean average of marks: " << mean;
+  cout << "\nStandard deviation of marks: " << standard_deviation;
+  cout << "\nStandard error of marks: " << standard_error;
 }
 
-void print_data(vector<Course> data, string scope)
+void print_data(vector<Course>& data, string subset, vector<int>& bad_lines)
 {
-    int row_count = data.size();
-    cout << "\n\n\033[1m" << scope << " list of data:\n\n\033[0m";
-    for (int i = 0; i < row_count; i++)
+  // Prints values from a subset of a given dataset
+  int row_count = data.size();
+  cout << "\n\n\033[1m" << subset << " list of data:\n\n\033[0m";
+  for (int i = 0; i < row_count; i++)
+  {
+    cout << data[i].name  << " ("
+         << data[i].id << "): " 
+         << data[i].mark << "\n";
+  }
+
+  if (!bad_lines.empty())
+  {
+    cout << "\033[1;33mWarning: Couldn't read data from following lines: ";
+    for (int i = 0; i < bad_lines.size(); i++)
     {
-        cout << data[i].name  << " ("
-             << data[i].id << "): " 
-             << data[i].mark << "\n";
+      cout << bad_lines[i] << ", ";
     }
 
-    analyse_data(data);
+    cout << " | these lines have been skipped\033[0m\n";
+  }
 
+  analyse_data(data);
 }
 
 int main()
 {
-    string const file_name = "course_marks.dat"; 
+  string const file_name = "course_marks.dat";
+  vector<int> bad_lines;
+  vector<Course> data = read_file(file_name, bad_lines);
 
-    string message = "Should the data be ordered by name instead of ID (Y/N)?";
+  string message = "Should the data be ordered by name, ID, or mark (n/i/m)?: ";
 
-    char extra_input;
-    char ordered = get_char_input(message, extra_input);
-    // Character input validation
-    while ((ordered != 'y' && ordered != 'n') || extra_input != '\n')
+  char extra_input;
+  char order = get_char_input(message, extra_input);
+  // Character input validation
+  while ((order != 'n' && order != 'i' && order != 'm') || extra_input != '\n')
+  {
+    // Ignore extra letters - only need to look at first two
+    while (extra_input != '\n' && cin.get(extra_input));
+
+    cout << "\033[1;31mError: Input must be either 'n', 'i', 'm' \033[0m\n";
+    order = get_char_input(message, extra_input);
+  }
+
+  // Sorts data based on required value using lambda function
+  if (order != 'i')
+  {
+    sort(data.begin(), data.end(),
+    [order](const Course& a, const Course& b)
     {
-      // Ignore extra letters - only need to look at first two
-      while (extra_input != '\n' && cin.get(extra_input));
+      if (order == 'n')
+        return a.name < b.name;
+      else
+        return a.mark < b.mark;
+    });
+  }
 
-      cout << "\033[1;31mError: Input must be either 'j' or 'e' \033[0m\n";
-      ordered = get_char_input(message, extra_input);
+  int row_count = data.size();
+  print_data(data, "Full", bad_lines);
+  cout << "\nThis file has a total of " << row_count << " entries.";
+
+  message = "\nFor what year would you like the details of?: ";
+  int target_year = get_integer_input(message);
+
+  vector<Course> year_data;
+  for (int i = 0; i < row_count; i++)
+  {
+    int target_value = data[i].id;
+    while (target_value >= 10)
+    {
+        target_value /= 10;
     }
 
-    vector<Course> data = read_file(file_name);
-    int row_count = data.size();
-
-    if (ordered == 'y')
-        sort(data.begin(), data.end(), compare_courses);
-    
-    print_data(data, "Full");
-    cout << "\nThis file has a total of " << row_count << " entries.";
-
-    message = "\nFor what year would you like the details of?: ";
-    int target_year = get_integer_input(message);
-
-    vector<Course> year_data;
-    for (int i = 0; i < row_count; i++)
+    if (target_value == target_year)
     {
-        int target_value = data[i].id;
-        while (target_value >= 10)
-        {
-            target_value /= 10;
-        }
-
-        if (target_value == target_year)
-        {
-            year_data.push_back(data[i]);
-        }
+        year_data.push_back(data[i]);
     }
+  }
 
-    row_count = year_data.size();
-    print_data(year_data, "Year");
-    cout << "\nThis year has a total of " << row_count << " entries.";
+  row_count = year_data.size();
+  bad_lines.clear();
+  print_data(year_data, "Year", bad_lines);
+  cout << "\nThis year has a total of " << row_count << " entries.";
 
-    return 0;
+  return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # CourseMarksData
 Reads, analyses and outputs student mark data from a .dat file
+
+### Statement on the use of AI
+No generative AI was used in the writing of or debugging of this code, all code was written and debugged by me with the use of the internet.
+
+### Description of devlopment timeline
+
+Created a base program that reads a given .dat file and stores the data into 3 vectors (1 per column). Changed to using a struct which contained the three vectors to more compactly store the data and make it easier to create a separate function to read the file.
+
+Impleneted std and stE calculations as separate functions for code cleanliness. Then filtered data for a specified year and printed out std and stE again for the new set. 
+
+Initially I read each column as a n-dimensional vector - meaning 3 different vectors for the data set, but this was cumbersome and made sorting by name difficult. I therefore switched to using a struct for each course, with a name, id, and mark attribute. Therefore only one n-dimensional vector was required using this "Course" data type, each element representing one row of data (one course). This allowed for cleaner code and for significantly easier sorting of data.
+
+I then added better data validation, as before it would just stop reading the file as soon as it encountered a row with wrong data types in. Now it simply skips those lines and warns the user which lines were unable to be read. Also made program terminate if file could not be opened at all. I decided to add sort by mark as an easy quick to implement feature as well.
